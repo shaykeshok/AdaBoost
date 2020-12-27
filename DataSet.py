3170277efce4cf432f5169b27e8721b6b0885f52
shaykeshok <shok.shayke@gmail.com>
2020-12-27 15:32:24 +0200

    success get answer

--- DataSet.py
+++ DataSet.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.utils import shuffle
 
 
 class DataSet:
@@ -20,14 +19,18 @@ class DataSet:
             df = df[df['label'] != "Iris-setosa"]
             df['label'] = df['label'].map({'Iris-versicolor': 1, 'Iris-virginica': -1})
             df[['label']] = df[['label']].astype(int)
+            df['weight'] = 0
         else:
             df.columns = ['x', 'label', 'y']
             df[['x', 'y']] = df[['x', 'y']].astype(float)
             df[['label']] = df[['label']].astype(int)
             df['label'] = df['label'].map({2: -1, 1: 1})
-        df['weight'] = 0
-        self.df = df
-        # df.sample(frac=1)
+            df['weight'] = 0
+            colnames = df.columns.tolist()
+            colnames = [colnames[0]] + [colnames[2]] + [colnames[1]] + [colnames[3]]
+            df = df[colnames]
+        lst = df.values
+        self.df = lst
 
     def print_data(self):
         print(self.df)
--- Line.py
+++ Line.py
@@ -1,42 +1,42 @@
 import Point
 
 
+def find_gradient(a, b, c, d):
+    try:
+        return (a - b) / (c - d)
+    except ZeroDivisionError:
+        return 0
+
+
 def find_vertical_line(point_a, point_b):
-    gradient = ((point_b.get_x - point_a.get_x) / (point_b.get_y - point_a.get_y)) * -1
-    x_c = (point_a.get_x + point_b.get_x) / 2
-    y_c = (point_a.get_y + point_b.get_y) / 2
-    return Point(x_c, y_c), gradient
+    # print(float(point_b['x']), float(point_a['x']), float(point_b['y']),float(point_a['y']))
+    # gradient: float = find_gradient(float(point_b['x']), float(point_a['x']), float(point_b['y']),
+    #                                 float(point_a['y'])) * -1
+    gradient: float = find_gradient(point_b[0], point_a[0], point_b[1], point_a[1]) * -1
+    # x_c = (float(point_a['x']) + float(point_b['x'])) / 2
+    # y_c = (float(point_a['y']) + float(point_b['y'])) / 2
+    # return x_c, y_c, \
+    return gradient
 
 
 # line (=rule) that separator between two points.
 class Line:
     def __init__(self, point_a, point_b):
-        point, gradient = find_vertical_line(point_a, point_b)
-        self.point = point
+        # print('point_a:', point_a, ' \npoint_b:', point_b)
+        gradient = find_gradient(point_b[0], point_a[0], point_b[1], point_a[1]) * -1
+        #     gradient = find_vertical_line(point_a, point_b)
         self.gradient = gradient
+        # self.inverse_gradient = find_gradient(float(point_b['y']), float(point_a['y']), float(point_b['x']),
+        #                                       float(point_a['x']))
+        self.inverse_gradient = find_gradient(point_b[1], point_a[1], point_b[0], point_a[0])
+        self.point_a = point_a
+        self.point_b = point_b
 
     # func to check the label of the point in relation to this rule
-    def point_labeling(self, point_to_label: Point):
-        side = point_to_label.get_x() * self.get_a() + self.get_b() - point_to_label.get_y()
+    def point_labeling(self, point_to_label):
+        # side = float(point_to_label['x']) * self.inverse_gradient + (
+        #         float(self.point_a['y']) - (self.inverse_gradient * float(self.point_a['x']))) - float(
+        #     point_to_label['y'])
+        side = point_to_label[0] * self.inverse_gradient + (
+                 self.point_a[1] - (self.inverse_gradient * self.point_a[0])) - point_to_label[1]
         return -1 if (side < 0) else 1
-
-
-def Line_hyp(points):
-    comb = combinations(points, 2)
-    err_sum = 1  # min sum of errors
-    max_rect = rec_hyp(point(), point())  # the hypothesis
-    for tupx in comb:
-        rect = rec_hyp(tupx[0], tupx[1])
-        temp_sum = 0  # What inside the hypothesis is 1
-        neg_temp_sum = 0  # What inside the hypothesis is -1
-        for x in points:
-            temp_sum += x.weight * int(x.gender != rect.include(x, 1))
-            neg_temp_sum += x.weight * int(x.gender != rect.include(x, -1))
-        if neg_temp_sum < temp_sum:
-            temp_sum = neg_temp_sum
-            rect.gender_classifier = -1
-        if temp_sum < err_sum:
-            err_sum = temp_sum
-            max_rect = rect
-    # print(err_sum)
-    return (max_rect, err_sum)
